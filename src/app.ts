4a86817ebb0f371e5f9936fe547b6b638f65dd2e
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-21 16:18:41 +0600

    thorw error message while route not found

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { ProductRoute } from "./app/modules/product/product.route";
 import { OrderRoute } from "./app/modules/order/order.route";
@@ -14,6 +14,22 @@ app.use("/api", OrderRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+// Not Found Route Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "An unexpected error occurred",
+  });
 });
 
 console.log(process.cwd());
